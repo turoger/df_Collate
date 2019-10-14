cf3efa1c275e23774401df323b2977df75180107
turoger <rogertu@scripps.edu>
2019-10-14 23:37:47 +0000

    first commit

---
+++ readME.md
@@ -0,0 +1,2 @@
+# This is a work in progress.
+License: BSD3
---
+++ setup.py
@@ -0,0 +1,22 @@
+
+from distutils.core import setup
+
+setup(
+    name='df_Collate',
+    version='0.0.0',
+    author='Roger Tu',
+    author_email='rogertu@scripps.edu',
+    url='https://github.com/turoger/df_Collate',
+    description='Library for working with UKBB dataframe',
+    license='BSD3',
+    keywords='UKBB GeneATLAS',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'License :: OSI Approved :: BSD3 License',
+        'Programming Language :: Python :: 3',
+    ],
+    packages=['df_Collate'],
+)
+
